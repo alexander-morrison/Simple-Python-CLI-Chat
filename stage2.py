7d3cf704c0b725a75c23abe22f76f7bef47ad8bf
Alexander Morrison <1611.developer@gmail.com>
2026-03-27 10:55:30 -0700

    setup

---
+++ stage2.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+# Initialize the Client
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))                 
+
+# Create Chat Completion
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {   # system sets behavior (personality, tone, restrictions)
+            "role": "system",
+            "content":  "You are a helpful assistant..."
+        },
+        {
+            # user message
+            "role": "user",
+            # actual user input
+            "content": "What are you?"
+        }
+    ]
+)
+
+# Print User Line
+print("You: What are you?")
+# Print Assistant Response
+print("Assistant:", response.choices[0].message.content)
